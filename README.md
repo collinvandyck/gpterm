69c68d1f0096cc41037eb27a0bbbbc8108009ba0
Collin Van Dyck <collin@ngrok.com>
2023-03-19 14:33:12 -0400

    By default gpterm will launch the UI.

--- README.md
+++ README.md
@@ -1,26 +1,37 @@
-# gpterm
+# gpterm (GPT terminal)
 
 gpterm is a terminal-based integraiton with the OpenAI chat APIs.
 
-It's very much a work in progress and is under active development.
+Chat history and other metadata are stored in
 
-# installation
+	~/.config/gpterm
 
-## from source
+# Installation
+
+Requirements:
+
+- Go 1.18 or higher
+- OpenAPI API Key
+
+To install from source:
 
 	git clone https://github.com/collinvandyck/gpterm.git
 	cd gpterm
 	make install
 
-# usage
+# Getting Started
+
+Because it uses the OpenAPI API, and API key is required before you can start:
 
 	# set api key
 	gpterm auth
 
-	# enter repl
-	gpterm repl
+Once the API key has been set, have fun!
+
+	# enter an interactive session
+	gpterm
 	
-# contributing
+# Contributing
 
 It's too early to accept pull requests given how early this project is. Please feel free to file an issue if you
 found a bug or have a feature request.
--- cmd/gpterm/root.go
+++ cmd/gpterm/root.go
@@ -1,24 +1,67 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"os"
 	"path/filepath"
 
 	"github.com/collinvandyck/gpterm/cmd/gpterm/cmd"
 	"github.com/collinvandyck/gpterm/cmd/gpterm/cmd/db"
 	"github.com/collinvandyck/gpterm/cmd/gpterm/cmd/exp"
+	"github.com/collinvandyck/gpterm/lib/client"
+	"github.com/collinvandyck/gpterm/lib/log"
+	"github.com/collinvandyck/gpterm/lib/store"
+	"github.com/collinvandyck/gpterm/lib/ui"
 	"github.com/spf13/cobra"
 )
 
+var logfile string
 var root = &cobra.Command{
 	Use:          filepath.Base(os.Args[0]),
-	Short:        "gpterm is a CLI that integrates with OpenAI",
+	Short:        "Start an interactive session with ChatGPT",
+	Long:         "Long",
+	Example:      "Example",
 	SilenceUsage: true,
+	Aliases:      []string{"repl"},
+	RunE: func(cmd *cobra.Command, args []string) error {
+		ctx := context.Background()
+		store, err := store.New()
+		if err != nil {
+			return fmt.Errorf("new store: %w", err)
+		}
+		key, err := store.GetAPIKey(ctx)
+		if err != nil {
+			return err
+		}
+		if key == "" {
+			fmt.Fprintln(os.Stderr, "No API key has been set. Run this command to set it:")
+			fmt.Fprintln(os.Stderr, "")
+			fmt.Fprintln(os.Stderr, fmt.Sprintf("%s auth", cmd.Root().Use))
+			os.Exit(1)
+		}
+		client, err := client.New(key)
+		if err != nil {
+			return fmt.Errorf("new client: %w", err)
+		}
+		logger := log.Discard
+		if logfile != "" {
+			f, err := os.Create(logfile)
+			if err != nil {
+				return err
+			}
+			defer f.Close()
+			logger = log.New(log.WithStdout(f), log.WithStderr(f))
+		}
+		ui := ui.New(store, client, ui.WithLogger(logger))
+		return ui.Run(ctx)
+	},
 }
 
 func init() {
+	root.Flags().StringVar(&logfile, "log", "", "log to this file")
+
 	root.AddCommand(cmd.Auth())
-	root.AddCommand(cmd.Repl())
 	root.AddCommand(cmd.Deps())
 	root.AddCommand(cmd.Usage())
 	root.AddCommand(db.DB(cmd.Deps()))
