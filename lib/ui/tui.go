714803784daf7407150365702d00322dee221723
Collin Van Dyck <collinvandyck@gmail.com>
2023-06-04 12:22:15 -0400

    wip

--- lib/ui/tui.go
+++ lib/ui/tui.go
@@ -14,39 +14,44 @@ const (
 var _ tea.Model = tuiModel{}
 
 type tuiModel struct {
+	uiOpts
 	state     tuiState
 	chatModel chatModel
 }
 
 func newTUIModel(opts uiOpts) tuiModel {
 	return tuiModel{
+		uiOpts:    opts.NamedLogger("tui"),
 		chatModel: newChatModel(opts),
 	}
 }
 
 // Init implements tea.Model.
 func (m tuiModel) Init() tea.Cmd {
+	m.Log("init")
 	return nil
 }
 
 // Update implements tea.Model.
 func (m tuiModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-	var cmd tea.Cmd
-	switch m.state {
-
-	case stateInit:
+	var cmds []tea.Cmd
+	if m.state == stateInit {
 		m.state = stateChat
-		cmd = m.chatModel.Init()
-		return m, cmd
+		cmds = append(cmds, m.chatModel.Init())
+	}
+
+	switch m.state {
 
 	case stateChat:
 		model, cmd := m.chatModel.Update(msg)
 		m.chatModel = model.(chatModel)
-		return m, cmd
+		cmds = append(cmds, cmd)
+		return m, tea.Sequence(cmds...)
 
 	default:
-		cmd = tea.Println("unhandled state")
-		return m, tea.Sequence(cmd, tea.Quit)
+		cmds = append(cmds, tea.Println("unhandled state"))
+		cmds = append(cmds, tea.Quit)
+		return m, tea.Sequence(cmds...)
 	}
 }
 
