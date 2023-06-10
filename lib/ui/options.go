30fb6780eaf56be7ecda4d33c051076323bfaea4
Collin Van Dyck <collinvandyck@gmail.com>
2023-06-10 13:55:15 -0400

    wip

--- lib/ui/options.go
+++ lib/ui/options.go
@@ -12,22 +12,43 @@ type optionsModel struct {
 	uiOpts
 	width   int
 	height  int
-	options []option
+	options options
 }
 
 type option struct {
-	name string
+	name   string
+	active bool
+}
+
+type options []option
+
+func (o options) move(direction int) {
+	for i, option := range o {
+		if option.active {
+			o[i].active = false
+			o[(len(o)+i+direction)%len(o)].active = true
+			return
+		}
+	}
 }
 
 func newOptionsModel(opts uiOpts) optionsModel {
-	return optionsModel{
+	model := optionsModel{
 		uiOpts: opts.NamedLogger("options"),
-		options: []option{
-			{
-				name: "api key",
-			},
+	}
+	model.options = options{
+		{
+			name:   "api key",
+			active: true,
+		},
+		{
+			name: "two",
+		},
+		{
+			name: "three",
 		},
 	}
+	return model
 }
 
 // Init implements tea.Model.
@@ -42,6 +63,10 @@ func (o optionsModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		o.width, o.height = msg.Width, msg.Height
 	case tea.KeyMsg:
 		switch msg.String() {
+		case "j", "down":
+			o.options.move(1)
+		case "k", "up":
+			o.options.move(-1)
 		}
 	}
 	return o, nil
@@ -56,6 +81,18 @@ func (o optionsModel) View() string {
 	header2 := styleHeader.Render(time.Now().Truncate(time.Second).String())
 	header := lipgloss.JoinVertical(lipgloss.Top, header1, header2)
 	doc.WriteString(header)
+	doc.WriteString("\n\n")
+
+	var styleListItem = lipgloss.NewStyle().Foreground(lipgloss.Color("#abc"))
+	var styleListItemActive = styleListItem.Copy().Foreground(lipgloss.Color("#fff")).Background(lipgloss.Color("#abc"))
+	for _, item := range o.options {
+		if item.active {
+			doc.WriteString(styleListItemActive.Render(item.name))
+		} else {
+			doc.WriteString(styleListItem.Render(item.name))
+		}
+		doc.WriteString("\n")
+	}
 
 	return lipgloss.NewStyle().Margin(2).Render(doc.String())
 }
