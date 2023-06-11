4f618e5d3ee2d768664235ef5d3d615c444ffa36
Collin Van Dyck <collinvandyck@gmail.com>
2023-06-11 17:15:39 -0400

    wip

--- lib/ui/model.go
+++ lib/ui/model.go
@@ -16,6 +16,16 @@ type model interface {
 	View() string
 }
 
+// composed model is a model that also has focus and blur methods.
+// this is designed for models which compose other models, but still
+// act somewhat generically. in other words, we don't want the composing
+// model to be too tightly coupled to the composed model.
+type composedModel interface {
+	model
+	Focus()
+	Blur()
+}
+
 // baseModel is a model that allows common data to be tracked.
 type baseModel struct {
 	width  int
