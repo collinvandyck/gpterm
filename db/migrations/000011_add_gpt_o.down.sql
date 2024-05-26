4185a30f776a72bacb30f2dd73facd9c92e48b5c
Collin Van Dyck <collinvandyck@gmail.com>
2024-05-26 15:33:14 -0400

    Set default model to gpt-4o.

---
+++ db/migrations/000011_add_gpt_o.down.sql
@@ -0,0 +1,3 @@
+delete from client_config where name = 'gpt-4o';
+update config set value='gpt-4-turbo-preview' where name='client-config';
+
---
+++ db/migrations/000011_add_gpt_o.up.sql
@@ -0,0 +1,3 @@
+insert into client_config (name, model, message_context) values ('gpt-4o', 'gpt-4o', 20);
+update config set value='gpt-4o' where name='client-config';
+
